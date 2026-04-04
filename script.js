c1133396d8cc93088707b30cb3ed2265a6eafcaa
beau <beau@beaus-MacBook-Pro.local>
2026-04-04 00:06:14 -0400

    added about me and functionality

--- script.js
+++ script.js
@@ -2,4 +2,29 @@ let skillsVal = false;
 let experienceVal = false;
 let educationVal = false;
 
-let skills = document.getElementById("skills-content");
+let skills = document.getElementById("skills-container");
+let education = document.getElementById("education-container");
+let experience = document.getElementById("experience-container");
+
+skills.style.display = 'flex';
+
+function showSkills()
+{
+    skills.style.display = 'flex';
+    education.style.display = 'none';
+    experience.style.display = 'none';
+}
+
+function showEducation()
+{
+    skills.style.display = 'none';
+    education.style.display = 'flex';
+    experience.style.display = 'none';
+}
+
+function showExperience()
+{
+    skills.style.display = 'none';
+    education.style.display = 'none';
+    experience.style.display = 'flex';
+}
