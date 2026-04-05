f3585f32f04663d00e41fc06a4e1cc60e8c13580
beau <beau@beaus-MacBook-Pro.local>
2026-04-05 14:19:18 -0400

    added transitions

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-let skillsVal = false;
-let experienceVal = false;
-let educationVal = false;
-
 let skills = document.getElementById("skills-container");
 let education = document.getElementById("education-container");
 let experience = document.getElementById("experience-container");
 
+let skillsTab =document.getElementById("skills-tab");
+let experienceTab = document.getElementById("experience-tab");
+let educationTab = document.getElementById("education-tab");
+
 skills.style.display = 'flex';
+skillsTab.classList.add('active-tab');
+
+function clearActiveTab(){
+    skillsTab.classList.remove('active-tab');
+    educationTab.classList.remove('active-tab');
+    experienceTab.classList.remove('active-tab');
+}
 
 function showSkills()
 {
     skills.style.display = 'flex';
     education.style.display = 'none';
     experience.style.display = 'none';
+    clearActiveTab();
+    skillsTab.classList.add('active-tab');
 }
 
 function showEducation()
@@ -20,6 +29,8 @@ function showEducation()
     skills.style.display = 'none';
     education.style.display = 'flex';
     experience.style.display = 'none';
+    clearActiveTab();
+    educationTab.classList.add('active-tab');
 }
 
 function showExperience()
@@ -27,4 +38,6 @@ function showExperience()
     skills.style.display = 'none';
     education.style.display = 'none';
     experience.style.display = 'flex';
+    clearActiveTab();
+    experienceTab.classList.add('active-tab');
 }
